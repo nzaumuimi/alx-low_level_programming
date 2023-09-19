5dd0a03319117d619f416ea4718101eef17f741f
nzaumuimi <nzaumuimi@gmail.com>
2023-09-19 12:53:49 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description : prints -putchar t stdout
+ * Return: always zero
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
