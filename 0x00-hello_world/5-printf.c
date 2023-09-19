281187f7ef605fa9fa207334ec2090ebb8bca178
nzaumuimi <nzaumuimi@gmail.com>
2023-09-19 12:18:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - use the function printf
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
