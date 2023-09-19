49219b616f3a60179d8aae2a204d88dfd22d52e7
nzaumuimi <nzaumuimi@gmail.com>
2023-09-19 12:38:25 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * print alphabets excepts letter e and q
+ * Return: alaways zero
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
