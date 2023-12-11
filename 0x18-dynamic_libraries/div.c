45d726f00fa70b9df51c07d84e4c6f895fc29c61
nzaumuimi <nzaumuimi@gmail.com>
2023-12-11 09:13:00 -0800

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * div - devide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division result
+ */
+int div(int a, int b)
+{
+	return(a / b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - module of two integers
+ * @a: first int
+ * @b: second int
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return(a % b);
+}
